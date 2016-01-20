792575c6677d07c681048c7e5988a6952686de28
maritzalapa <maritzalapa@gmail.com>
2016-01-20 12:55:49 -0500

    Subiendo laboratorio 3

---
+++ PrimeraAplicacion/app/src/main/java/com/example/noa/primeraaplicacion/SegundaActividad.java
@@ -0,0 +1,32 @@
+package com.example.noa.primeraaplicacion;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by noa on 17/01/16.
+ */
+public class SegundaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_actividad);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with yoxur own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
